9cd93e18ed8fb3f7089597e9b2bd637cd74bd63c
Hamzhgg <gharibehhamzh@gmail.com>
2024-11-26 16:05:44 +0300

    holaa

--- exercise.py
+++ exercise.py
@@ -1,13 +1,31 @@
+# Exercise 0: Example
+#
+# This is a practice exercise to help you understand how to write code "inside" a provided Python function.
+#
+# We'll create a function that checks a condition and prints a specific greeting message based on that condition.
+#
+# Requirements:
+# - The function is named `print_greeting`.
+# - Inside the function, declare a variable `python_is_fun` and set it to `True`.
+# - Use a conditional statement to check if `python_is_fun` is `True`.
+# - If `python_is_fun` is `True`, print the message "Python is fun!"
 
+def print_greeting():
+    # Your code goes here. Remember to indent!
+    python_is_fun = True
+    if python_is_fun:
+        print("Python is fun!")
+
+# Call the function
+print_greeting()
+
+print('-----------------------------------------------------------')
 
 
 def check_letter():
-    # Prompt the user to enter a letter (either uppercase or lowercase)
-    letter = input("Enter a letter (a-z or A-Z): ").lower()  # Convert to lowercase to simplify checking
+    letter = input("Enter a letter (a-z or A-Z): ").lower()
 
-    # Check if the input is a single alphabetical character
     if len(letter) == 1 and letter.isalpha():
-        # Check if the letter is a vowel
         if letter in "aeiou":
             print(f"The letter {letter} is a vowel.")
         else:
@@ -15,7 +33,28 @@ def check_letter():
     else:
         print("Invalid input. Please enter a single alphabetical character.")
 
-# Call the function to run the code
 check_letter()
+
+
+('-----------------------------------------------------------')
+
+
+def check_voting_eligibility():
+    try:
+        age = int(input("Please enter your age: "))
+        if age < 0:
+            print("Age cannot be negative!")
+        else:
+            if age >= 18:
+                print("You are eligible to vote.")
+            else:
+                print("You are not eligible to vote.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+# Call the function
+check_voting_eligibility()
+
+('-----------------------------------------------------------')
 
 
